0c2e1b86f4a81831e914e4fce46e40a26e616c0e
BALAJI <142514847+balaji-fullstackdeveloper@users.noreply.github.com>
2024-09-23 17:10:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 const cors = require("cors");
-app.use(cors({ origin: "https://main--thunderous-croissant-c12998.netlify.app" }))
+app.use(cors({ credentials: true,origin: "https://main--thunderous-croissant-c12998.netlify.app" }))
 app.use((req, res, next) => {
   res.setHeader(
     "Cache-Control",
