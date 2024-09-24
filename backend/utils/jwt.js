35b8feca0779703312839abd1813180666e540f1
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-09-24 15:15:54 +0530

    commits

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,26 +1,22 @@
 const sendToken = (user, statusCode, res) => {
+  //Creating JWT Token
+  const token = user.getJwtToken();
 
-    //Creating JWT Token
-    const token = user.getJwtToken();
+  //setting cookies
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    sameSite: true,
+    httpOnly: true,
+    secure: true,
+  };
 
-    //setting cookies 
-    const options = {
-        expires: new Date(
-                Date.now() + process.env.COOKIE_EXPIRES_TIME  * 24 * 60 * 60 * 1000 
-            ),
-        sameSite:"none"
-     
-    }
-
-    res.status(statusCode)
-    .cookie('token', token, options)
-    .json({
-        success: true,
-        token,
-        user
-    })
-
-
-}
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
 module.exports = sendToken;
