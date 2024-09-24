7eeb8eb0c7e770fe37ba5762ff8a81c9a97a2dbb
BALAJI <142514847+balaji-fullstackdeveloper@users.noreply.github.com>
2024-09-24 15:46:29 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
-    sameSite: true,
+    sameSite: "none",
     httpOnly: true,
     secure: true,
   };
