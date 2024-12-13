06350d1f9508011f76e329b2ff0b5188fd4a0215
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-12-13 10:06:53 +0530

    Push

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
